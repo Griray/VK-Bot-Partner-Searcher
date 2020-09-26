1786c463088cd2dbcaf73488ea18bc5c98f79762
Grigorii Raikov <griray090494@gmail.com>
2020-09-26 23:53:44 +0300

    Code correction, add def sending photos, add def filter id list of partners, add def cycle sending three photos

--- Partner_Searcher_Vk_Bot.py
+++ Partner_Searcher_Vk_Bot.py
@@ -1,9 +1,8 @@
 from vk_api.longpoll import VkLongPoll, VkEventType
 from bs4 import BeautifulSoup as bs
 from datetime import datetime
-from text_book import status, age_from, age_to, gender, city, check, congratulations
+from text_book import status, age_from, age_to, gender, city, check, congratulations, do_not_worry
 import vk_api
-from vk_api import VkUpload
 import requests
 import random
 import time
@@ -23,19 +22,6 @@ def get_vk_name(user_id):
     return name.split()[0]
 
 
-def get_vk_id(user_id):
-    response = requests.get(
-        "https://api.vk.com/method/users.get",
-        params={
-            "access_token": token,
-            "v": 5.89,
-            "user_ids": user_id,
-        }
-    )
-    id_nick_json = response.json()
-    return id_nick_json['response'][0].get('id')
-
-
 # Функцию отправки фото ботом
 def send_photo(user_id, message, attachment=None):
     kw = {'user_id': user_id, 'message': message, 'random_id': random.randrange(10 ** 7)}
@@ -44,18 +30,19 @@ def send_photo(user_id, message, attachment=None):
     vk_session.method('messages.send', kw)
 
 
+# Функцию отправки сообщения ботом
 def write_message(user_id, message, random_id):
     vk_session.method('messages.send', {'user_id': user_id, 'message': message, 'random_id': random_id})
 
 
-# Функция поиска id людей, которые подошли под запросы елиента бота
+# Функция поиска id людей, которые подошли под запросы клиента
 def search_for_partner():
     partner_response = requests.get(
         "https://api.vk.com/method/users.search",
         params={
             "access_token": access_token,
             "sort": 1,
-            "count": 20,
+            "count": 150,
             "hometown": criterian[3],
             "sex": criterian[2],
             "status": criterian[4],
@@ -69,14 +56,14 @@ def search_for_partner():
     time.sleep(0.34)
     id_list = []
     for dicts in partners['response']['items']:
-        if dicts.get('is_closed') is False:
+        if dicts.get('is_closed') is False and dicts.get('can_access_closed') is True:
             id_list.append(dicts.get('id'))
     return id_list
 
 
-# Функция на получение url фотографий подходящих партнеров, ID которых отобраны в функции search_for_partner
-def get_partner_photos():
-    for id in search_for_partner():
+# Фильтрует список id, убирает id с кол-вом фото < 3
+def clean_id_with_less_photos(id_list):
+    for id in id_list:
         photo_response = requests.get(
             "https://api.vk.com/method/photos.get",
             params={
@@ -90,79 +77,44 @@ def get_partner_photos():
         )
         photo_json = photo_response.json()
         time.sleep(0.34)
-        jpg_links = []
-        if photo_json['response'].get('count') == 3:
-            for links in photo_json['response']['items']:
-                jpg_links.append(links.get('sizes')[-1].get('url'))
-            return jpg_links
-        elif photo_json['response'].get('count') > 3:
-            photo_links = []
-            count_likes = []
-            for links in photo_json['response']['items']:
-                photo_links.append(links.get('sizes')[-1].get('url'))
-            for count in photo_json['response']['items']:
-                count_likes.append(count.get('likes').get('count'))
-            tup = sorted(zip(photo_links, count_likes), reverse=True)[:3]
-            for links in tup:
-                jpg_links.append(links[0])
-            return jpg_links
-        else:
-            continue
+        if photo_json['response'].get('count') < 3:
+            id_list.remove(id)
+    return id_list
 
 
 # Функция выводящая имя фотографии в формате для отправки во вложении к сообщению
-def get_photos_name():
-    for id in search_for_partner():
-        photo_response = requests.get(
-            "https://api.vk.com/method/photos.get",
-            params={
-                "access_token": access_token,
-                "v": 5.77,
-                "owner_id": id,
-                "album_id": "profile",
-                "extended": 1,
-                "photo_sizes": 0,
-            }
-        )
-        photo_json = photo_response.json()
-        time.sleep(0.34)
-        names = []
-        if photo_json['response'].get('count') == 3:
-            for links in photo_json['response']['items']:
-                photo_name = 'photo' + str(links.get('owner_id')) + '_' + str(links.get('id'))
-                names.append(photo_name)
-                return names
-        elif photo_json['response'].get('count') > 3:
-            photo_links = []
-            count_likes = []
-            three_names = []
-            for links in photo_json['response']['items']:
-                photo_links.append(links.get('sizes')[-1].get('url'))
-            for count in photo_json['response']['items']:
-                count_likes.append(count.get('likes').get('count'))
-            for photo in photo_json['response']['items']:
-                name = 'photo' + str(photo.get('owner_id')) + '_' + str(photo.get('id'))
-                three_names.append(name)
-            tup = sorted(zip(photo_links, count_likes, three_names), reverse=True)[:3]
-            for links in tup:
-                names.append(links[2])
-                return names
-
-
-# Функция, сохраняющая фото на сервере??? Не очень понимаю нужна ли она все таки или нет.
-# В документации размыто написанно
-# def saving_photos_to_server_of_vk():
-#     photo_response = requests.get(
-#         "https://api.vk.com/method/photos.getMessagesUploadServer",
-#         params={
-#             "access_token": access_token,
-#             "peer_id": event.user_id,
-#             "v": 5.52
-#         }
-#     )
-#     res = photo_response.json()
-#     download_url = res.get('upload_url')
-#     return download_url
+def get_photos_name(vk_id):
+    photos = []
+    photo_response = requests.get(
+        "https://api.vk.com/method/photos.get",
+        params={
+            "access_token": access_token,
+            "v": 5.77,
+            "owner_id": vk_id,
+            "album_id": "profile",
+            "extended": 1,
+            "photo_sizes": 0,
+        }
+    )
+    photo_json = photo_response.json()
+    time.sleep(0.34)
+    if photo_json['response'].get('count') == 3:
+        for elements in photo_json['response'].get('items'):
+            photo_name = 'photo' + str(elements.get('owner_id')) + '_' + str(elements.get('id'))
+            photos.append(photo_name)
+        return photos
+    elif photo_json['response'].get('count') >= 3:
+        count_likes = []
+        three_names = []
+        for count in photo_json['response']['items']:
+            count_likes.append(count.get('likes').get('count'))
+        for photo in photo_json['response']['items']:
+            name = 'photo' + str(photo.get('owner_id')) + '_' + str(photo.get('id'))
+            three_names.append(name)
+        tup = sorted(zip(count_likes, three_names), reverse=True)[:3]
+        for links in tup:
+            photos.append(links[1])
+        return photos
 
 
 # Ожидает первого сообщения от пользователя
@@ -176,14 +128,7 @@ def start_conversation_greetings():
             if event.text == 'да':
                 if event.from_user:
                     write_message(identity, 'Начнём', random.randint(-2147483648, 2147483647))
-                elif event.from_chat:
-                    write_message(identity, 'Начнём', random.randint(-2147483648, 2147483647))
                     break
-            elif event.text == 'нет':
-                if event.from_user:
-                    write_message(identity, 'Я другого не умею', random.randint(-2147483648, 2147483647))
-                elif event.from_chat:
-                    write_message(identity, 'Я другого не умею', random.randint(-2147483648, 2147483647))
 
 
 # Выводит в консоль информацию по полученным сообщениям
@@ -196,50 +141,81 @@ def print_information_about_message(message_text):
 # Начало поиска + функция look_for_age_from
 def start_searching_age_from():
     write_message(identity, age_from, random.randint(-2147483648, 2147483647))
-    if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
-        message_text = event.text.lower()
-        print_information_about_message(message_text)
-        criterian.append(int(message_text))
+    for event in longpoll.listen():
+        if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
+            message_text = event.text.lower()
+            print_information_about_message(message_text)
+            criterian.append(int(message_text))
+            break
 
 
 # Поиск пратнера по параметру возраст до
 def look_for_age_to():
     write_message(identity, age_to, random.randint(-2147483648, 2147483647))
-    if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
-        message_text = event.text.lower()
-        print_information_about_message(message_text)
-        criterian.append(int(message_text))
+    for event in longpoll.listen():
+        if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
+            message_text = event.text.lower()
+            print_information_about_message(message_text)
+            criterian.append(int(message_text))
+            break
 
 
 # Поиск по параметру пол партнера
 def look_for_gender():
     write_message(identity, gender, random.randint(-2147483648, 2147483647))
-    if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
-        message_text = event.text.lower()
-        print_information_about_message(message_text)
-        criterian.append(int(message_text))
+    for event in longpoll.listen():
+        if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
+            message_text = event.text.lower()
+            print_information_about_message(message_text)
+            criterian.append(int(message_text))
+            break
 
 
 # Поиск по параметру семейное положение
 def look_for_status():
     write_message(identity, status, random.randint(-2147483648, 2147483647))
-    if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
-        message_text = event.text.lower()
-        print_information_about_message(message_text)
-        criterian.append(int(message_text))
+    for event in longpoll.listen():
+        if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
+            message_text = event.text.lower()
+            print_information_about_message(message_text)
+            criterian.append(int(message_text))
+            break
 
 
 # Поиск по параметру город
 def look_for_partners_city():
     write_message(identity, city, random.randint(-2147483648, 2147483647))
-    if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
-        message_text = event.text.lower()
-        print_information_about_message(message_text)
-        criterian.append(message_text)
+    for event in longpoll.listen():
+        if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
+            message_text = event.text.lower()
+            print_information_about_message(message_text)
+            criterian.append(message_text)
+            break
+
+
+def cycle_sending_three_photos(id_list):
+    for id in id_list:
+        photo_name = get_photos_name(id)
+        if photo_name is None:
+            continue
+        else:
+            for photo in photo_name:
+                send_photo(identity, '', attachment=photo)
+            for event in longpoll.listen():
+                if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
+                    event.text.lower()
+                    if event.text == 'да':
+                        if event.from_user:
+                            write_message(identity, 'https://vk.com/id' + str(id) + congratulations,
+                                          random.randint(-2147483648, 2147483647))
+                    elif event.text == 'нет':
+                        if event.from_user:
+                            write_message(identity, do_not_worry, random.randint(-2147483648, 2147483647))
+                            truely_partners.remove(id)
+        continue
 
 
 criterian = []
-
 for event in longpoll.listen():
     if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
         event.text.lower()
@@ -247,3 +223,14 @@ for event in longpoll.listen():
             identity = event.user_id
             start_conversation_greetings()
             start_searching_age_from()
+            look_for_age_to()
+            look_for_gender()
+            look_for_partners_city()
+            look_for_status()
+            print('Критерии отобранные клиентом', criterian)
+            all_partners = search_for_partner()
+            truely_partners = clean_id_with_less_photos(all_partners)
+            write_message(identity, check, random.randint(-2147483648, 2147483647))
+            cycle_sending_three_photos(truely_partners)
+
+
